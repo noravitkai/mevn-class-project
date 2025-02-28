ac747c17a42dadb396f731dffaffc22cb3e6eca3
noravitkai <norvit01@easv365.dk>
2025-02-28 14:45:45 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template class="w-screen">
   <header class="text-white py-4">
-    <div class="wrapper" >
-
+    <div class="wrapper">
       <nav class="">
         <RouterLink to="/" class="mr-4">Home</RouterLink>
         <RouterLink to="/about" class="mr-4">About</RouterLink>
 
         <!-- RouterLink to products, Auth, Admin & and logout button -->
 
-
         <!-- toggle cart button -->
 
-
         <!-- Routerlink to orders -->
-
-
       </nav>
     </div>
   </header>
@@ -22,15 +17,11 @@
   <RouterView />
 
   <!-- CartBasket component -->
-
-
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
-
 
 <style scoped>
 .logo {
